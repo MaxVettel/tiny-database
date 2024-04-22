77037bafbd9738f33d75871e8005f57c3351f23b
Igor <vettel.git@gmail.com>
2024-04-23 01:26:15 +0300

    TD Add writing in file in append mode

--- interpreter/commandHandler.go
+++ interpreter/commandHandler.go
@@ -3,10 +3,15 @@ package interpreter
 import (
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func HandleCommand(text string) {
-	switch text {
+	re := regexp.MustCompile(`("[^"]+"|[^\s"]+)`)
+	args := re.FindAllString(text, -1)
+	fmt.Println("Command:", args[0])
+	fmt.Println("Args:", args[1:])
+	switch args[0] {
 	case "stop":
 		os.Exit(0)
 	case "get":
@@ -14,7 +19,9 @@ func HandleCommand(text string) {
 	case "getall":
 		getall()
 	case "set":
-		set()
+		set(args[1:])
+	case "":
+		fmt.Println("empty command")
 	default:
 		fmt.Println("unknown command")
 	}
--- interpreter/commands.go
+++ interpreter/commands.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/google/uuid"
 )
+
+func getDatabaseFile(appendMode int) *os.File {
+	filePath, err := os.Getwd()
+	if err != nil {
+        log.Fatalf("Error with load database file path %v", err)
+    }
+	file, err := os.OpenFile(filePath + "/interpreter/database-file.txt", appendMode, 0600)
+    if err != nil {
+        log.Fatalf("Error with load database file %v", err)
+    }
+	return file
+}
 
 func get() {
 	fmt.Println("get value by id")
@@ -12,17 +26,19 @@ func get() {
 
 func getall() {
 	fmt.Println("get all pair id:value")
-	filePath, err := os.Getwd()
-	if err != nil {
-        log.Fatalf("Error with load database file path %v", err)
-    }
-	file, err := os.ReadFile(filePath + "/interpreter/database-file.txt")
-    if err != nil {
-        log.Fatalf("Error with load database file %v", err)
-    }
-	fmt.Println(string(file))
 }
 
-func set() {
+func set(args []string) {
 	fmt.Println("set value and return its id")
+	fmt.Println("set args:", args)
+	file := getDatabaseFile(os.O_APPEND)
+	defer file.Close()
+	//https://github.com/google/uuid
+	for i := range args {
+		n, err := file.WriteString(fmt.Sprintf("\nkey:%s,value:%s;", uuid.New().String(), args[i]))
+		fmt.Println("is written:", n)
+		if err != nil {
+			log.Fatalf("Error with writing in file %v", err)
+		}
+	}
 }
